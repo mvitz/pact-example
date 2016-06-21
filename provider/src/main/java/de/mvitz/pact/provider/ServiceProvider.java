34b75303d0acd22881adffdb769018af1b429765
Michael Vitz <michael.vitz@innoq.com>
2016-06-21 16:06:45 +0200

    Small provider cleanup

--- provider/src/main/java/de/mvitz/pact/provider/ServiceProvider.java
+++ provider/src/main/java/de/mvitz/pact/provider/ServiceProvider.java
@@ -2,8 +2,7 @@ package de.mvitz.pact.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
--- provider/src/test/java/de/mvitz/pact/provider/ServiceProviderPactIT.java
+++ provider/src/test/java/de/mvitz/pact/provider/ServiceProviderPactIT.java
@@ -1,11 +1,8 @@
 package de.mvitz.pact.provider;
 
-import au.com.dius.pact.provider.junit.PactRunner;
-import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.*;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
-import au.com.dius.pact.provider.junit.target.HttpTarget;
-import au.com.dius.pact.provider.junit.target.Target;
-import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.junit.target.*;
 import org.junit.runner.RunWith;
 
 @RunWith(PactRunner.class)
